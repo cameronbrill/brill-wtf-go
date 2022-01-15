92739265c7ad8e8374bfd60ebc7e9e8db4db6f1c
Cameron Brill <c00brill@gmail.com>
2022-01-14 19:19:01 -0800

    go fmt

--- cmd/link/main.go
+++ cmd/link/main.go
@@ -6,27 +6,27 @@ import (
 )
 
 func main() {
-        app := cli.App{
-        	Name:         "link",
-        	Usage:        cli.Usage("Generate a short url!"),
-        	Action:       cli.ActionFunc(func(cmd *cli.Command) cli.ActionRunner {
+	app := cli.App{
+		Name:  "link",
+		Usage: cli.Usage("Generate a short url!"),
+		Action: cli.ActionFunc(func(cmd *cli.Command) cli.ActionRunner {
 
-                        url :=  cli.StringArg(cmd, "url", cli.Usage("The URL you are shortening"))
+			url := cli.StringArg(cmd, "url", cli.Usage("The URL you are shortening"))
 
-                        return func(cmd *cli.Command) error { 
-                                l, err := link.New(link.URL(*url))
-                                if err != nil {
-                                        return err
-                                }
-                                cmd.Println("Visit ", (&l).String())
-                                return nil 
-                        }
-                }),
-                CommandFlags: []cli.CommandFlag{
+			return func(cmd *cli.Command) error {
+				l, err := link.New(link.URL(*url))
+				if err != nil {
+					return err
+				}
+				cmd.Println("Visit ", (&l).String())
+				return nil
+			}
+		}),
+		CommandFlags: []cli.CommandFlag{
 			cli.HelpCommandFlag(),
 			cli.VersionCommandFlag("0.0.0"),
 		},
-        }
+	}
 
-        app.HandleError(app.Run())
+	app.HandleError(app.Run())
 }
