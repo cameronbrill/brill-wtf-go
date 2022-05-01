17bd561643674ac46ced25c90b00cc7c66b538b4
Cameron Brill <c00brill@gmail.com>
2022-05-01 16:30:09 -0400

    Update service interface

--- service/interface.go
+++ service/interface.go
@@ -5,14 +5,15 @@ import "errors"
 // ErrNotFound signifies that a single requested object was not found.
 var ErrNotFound = errors.New("not found")
 
-// User is a user business object.
-type User struct {
-	ID   int64
-	Name string
+// Link is a Link business object.
+type Link struct {
+	ID       int64
+	Original string
+	Short    string
 }
 
 // Service defines the interface exposed by this package.
 type Service interface {
-	GetUser(id int64) (User, error)
-	GetUsers(ids []int64) (map[int64]User, error)
+	NewLink(string) (Link, error)
+	ShortURLToLink(string) (Link, error)
 }
