10b49f3fe6624de76e9fbaeae80bfa356bf897fe
Cameron Brill <c00brill@gmail.com>
2022-05-11 14:41:39 -0700

    enhance link context

--- rest/context.go
+++ rest/context.go
@@ -16,6 +16,9 @@ const (
 func linkCtx(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		link := chi.URLParam(r, "link")
+		if l := r.URL.Query().Get("link"); l != "" {
+			link = l
+		}
 		ctx := context.WithValue(r.Context(), linkKey, link)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
