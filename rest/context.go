0fec15555bd69671439cb80df2c611a5fde1b6fc
Cameron Brill <c00brill@gmail.com>
2022-05-11 11:56:49 -0700

    create type for context key

--- rest/context.go
+++ rest/context.go
@@ -7,10 +7,16 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+type key string
+
+const (
+	linkKey key = "link"
+)
+
 func linkCtx(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		link := chi.URLParam(r, "link")
-		ctx := context.WithValue(r.Context(), "link", link)
+		ctx := context.WithValue(r.Context(), linkKey, link)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
