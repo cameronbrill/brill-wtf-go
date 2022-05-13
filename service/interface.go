12d1fbee330c0d47c5f645212f350b6b9ad2b383
Cameron Brill <c00brill@gmail.com>
2022-05-12 22:32:37 -0700

    create error

--- service/interface.go
+++ service/interface.go
@@ -1,20 +1,14 @@
 package service
 
+import (
+	"errors"
+
 	"github.com/cameronbrill/brill-wtf-go/model"
 )
 
 // ErrNotFound signifies that a single requested object was not found.
 var ErrNotFound = errors.New("not found")
-
-// Link is a Link business object.
-type Link struct {
-	ID       int64
-	Original string
-	Short    string
-	want     string
-}
-
-type NewLinkOption func(*Link) error
+var ErrAlreadyExits = errors.New("already exists")
 
 // Service defines the interface exposed by this package.
 type Service interface {
