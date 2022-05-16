1a4aad76c355c568c64810cd37b056b12b3ec82c
Cameron Brill <c00brill@gmail.com>
2022-05-15 20:37:22 -0700

    feat: implement page renderer

--- web/options.go
+++ web/options.go
@@ -1,3 +1,26 @@
 package web
 
+import "time"
+
 type RenderOption func(*PageRenderer) error
+
+func WithShortURL(val string) RenderOption {
+	return func(r *PageRenderer) error {
+		r.data.Short = val
+		return nil
+	}
+}
+
+func WithOriginalURL(val string) RenderOption {
+	return func(r *PageRenderer) error {
+		r.data.Orig = val
+		return nil
+	}
+}
+
+func WithTTL(val time.Duration) RenderOption {
+	return func(r *PageRenderer) error {
+		r.data.TTL = val
+		return nil
+	}
+}
--- web/render.go
+++ web/render.go
@@ -1,7 +1,10 @@
 package web
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
+	"time"
 )
 
 type Renderer interface {
@@ -13,3 +16,38 @@ type Page string
 const (
 	NotFound Page = "404"
 )
+
+func New() Renderer {
+	return &PageRenderer{
+		data: &pageData{},
+	}
+}
+
+type PageRenderer struct {
+	data *pageData
+}
+
+type pageData struct {
+	Short string
+	Orig  string
+	TTL   time.Duration
+}
+
+func (p *PageRenderer) Render(w http.ResponseWriter, r *http.Request, page Page, opts ...RenderOption) {
+	for _, opt := range opts {
+		opt(p)
+	}
+
+	t, err := template.ParseFiles(fmt.Sprintf("web/%s.gohtml", page))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	err = t.Execute(w, p.data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
